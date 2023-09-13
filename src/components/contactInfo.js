a2309c4f8acee8b1ea654c32538b666406fb2106
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-09-13 06:02:23 +0300

    need to fix the map still

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -1,4 +1,3 @@
-// ContactInfo.js
 import React from 'react';
 
 const ContactInfo = () => (
@@ -18,7 +17,7 @@ const ContactInfo = () => (
       <h4>Call:</h4>
       <p><a href="tel:1-234-567-8910">+1-234-567-8910</a></p>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1835144.8375713965!2d34.250701024132056!3d32.01245562401206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502d7d634c1fc4b%3A0xd96f623e456ee1cb!2sJerusalem%2C%20Israel!5e0!3m2!1sen!2sus!4v1687942778547!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
+    <iframe title='Google Maps Location' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1835144.8375713965!2d34.250701024132056!3d32.01245562401206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502d7d634c1fc4b%3A0xd96f623e456ee1cb!2sJerusalem%2C%20Israel!5e0!3m2!1sen!2sus!4v1687942778547!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
   </div>
 );
 
