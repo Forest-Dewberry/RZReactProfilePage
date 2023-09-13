0273ce3feeb72d760dbac42efe8084a2a6efe0f1
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-09-13 06:04:36 +0300

    herosection typed works great

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 const HeroSection = () => {
+  const typedRef = useRef();
+
+  useEffect(() => {
+    const typed = new Typed(typedRef.current, {
+      strings: [
+        'a Data Analyst',
+        'learning UI Design',
+        'a Freelancer',
+        'a Helicopter Mechanic',
+        'a Website Developer',
+        'an Aerial Gunner',
+        'a Cat Lover',
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
-      <div className="hero-container" data-aos="fade-in">
+    <section
+      id='hero'
+      className='d-flex flex-column justify-content-center align-items-center'
+    >
+      <div className='hero-container' data-aos='fade-in'>
         <h1>Ryan Zierman</h1>
-        <p>I'm <span className="typed" data-typed-html="true" data-typed-items="a Data Analyst, learning UI Design, a Freelancer, a Helicopter Mechanic, a Website Developer, an Aerial Gunner, a Cat Lover"></span></p>
+        <p>
+          I'm <span ref={typedRef}></span>
+        </p>
       </div>
     </section>
   );
